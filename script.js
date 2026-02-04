1ac7ce0e29005e21c6334476d1c87efa375ca208
tombarker21 <23197tb@hvhs.school.nz>
2026-02-04 15:14:32 +1300

    wowzers its working

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+    const input =document.getElementById("input");
+
+    const tab =document.getElementById("tab");
+
+
 function callAlert() {
 alert("Congratulations!\nYou got the script running.\nfree ram guys right here ")
 
 };
+
+function customText () {
+
+    tab.innerHTML=input.value;
+
+}
